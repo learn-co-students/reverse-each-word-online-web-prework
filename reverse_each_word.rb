016a38b0db4b128412a082019638d5318f9f5e26
Roberto Samia <bojosteph@gmail.com>
2018-09-23 01:46:33 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,24 @@
+def reverse_each_word(array)
+  word = [sentence1, sentence2]
+  word.split = new_word
+  new_word.each do |sentence|
+      sentence.reverse!
+  return sentence
+  
+  new_word.collect do |sentence|
+      sentence.reverse!
+  return sentence
+  end
+ end
+end
+  
+
+
+
+
+  
+  
+
+
+
+
