c201e425d9b2c75192ba1901faf14a2707b6b8ff
Brandon Teich <brandonteich123@gmail.com>
2018-10-02 23:04:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,4 +4,10 @@ def reverse_each_word(sentence)
   new_array.join(" ")
 end 
 
-def reverse_each_word
+def reverse_each_word(sentence_string)
+  split_string = sentence_string.split(" ")
+  split_string.collect do |word|
+    word.reverse
+  end
+  split_string.join(" ")
+end
