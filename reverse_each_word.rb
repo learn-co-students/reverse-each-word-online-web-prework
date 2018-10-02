171d1ab2a17c61640e7985411a996af52dd6975f
Brandon Teich <brandonteich123@gmail.com>
2018-10-02 22:55:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word(sentence_string)
   end
   sentence_string
 end
+
+def reverse_each_word(sentence_string)
+  
+  sentence_string.each do |word|
+    sentence_string << word.reverse
+  end
+  sentence_string
+end
