7f9f0318b1c12d6a008bcd9063b9688084f5eb5a
Brandon Teich <brandonteich123@gmail.com>
2018-10-02 22:54:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,7 @@
-words = ["are", "any", "of", "these", "words", "longer", "than", "four", "letters"]
- 
-words.any? do |word|
-  word.length > 4
-end
-  #=> t
-  
-  
-  
 def reverse_each_word(sentence_string)
-  
   
   sentence_string.each do |word|
     sentence_string << word.reverse
   end
   sentence_string
 end
-  s = ".sdrawkcab si gnirts sihT"
-	s.reverse                            # => "This string is backwards."
-	s      
