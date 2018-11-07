4958c6f0bd0ab6e6785a0255abd9636dd5b1ef3f
Janita Madramootoo <jaymadr@gmail.com>
2018-11-07 20:07:42 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,12 @@
-def reverse_each_word(array)
-  array = array.split   #splits the sentance to array
-  rev_array = []  #new array to save the reversed words
-  array.each { |word| rev_array << word.reverse }
-  rev_array.join(" ")
-end
+# def reverse_each_word(array)
+#   array = array.split   #splits the sentance to array
+#   rev_array = []  #new array to save the reversed words
+#   array.each { |word| rev_array << word.reverse }
+#   rev_array.join(" ")
+# end
 
-def reverse_each_word(array)
-  array = array.split #splits the sentance to array
-  rev_array = [] #new array to save the reverse words??
-  array.collect { |word| rev_array << word.reverse }
-  rev_array.join(" ")
+def reverse_each_word(string)
+  array = string.split
+  array.collect { |word| word.reverse! }
+  array.join(" ")
 end
