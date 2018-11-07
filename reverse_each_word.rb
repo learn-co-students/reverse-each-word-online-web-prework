5f55c32a2f6f7a9fb82b6e2b86d80467d6358bf2
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-07 03:43:59 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
   array=str.split(" ")
-new_array =  array.collect do |element|
+  array.collect do |element|
     element.reverse
-  end
-  new_array.join(" ")
+  end.join(" ")
+
 end
