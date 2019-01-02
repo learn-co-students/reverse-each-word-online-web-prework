3ec5755ab61440d5c0a2ea7c988ebe3bc2bfa52a
Trent Moss <trentmoss05@gmail.com>
2019-01-02 15:45:26 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
-def reverse_each_word(string)
-  string.split(" ")
-  string.each do |words|
-    words.reverse_each
-
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
