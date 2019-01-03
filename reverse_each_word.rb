db744da5a5dd6e562b0bec39edfee1137f97c079
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-03 19:59:20 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
- string = string.split()
+ string = string.split(" ")
  reversed_string = []
-  string.each do |char|
-    reversed_string << char.reverse! 
+  string.collect do |string|
+    reversed_string << string.reverse
     end   
-  return reversed_string.join(' ')
+  return reversed_string.join (" ")
 end    
 
 
