b8ba8573cfae215a51b30b57e8213913bfa533f8
H B <phajib@gmail.com>
2019-01-20 00:13:23 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
-def reverse_each_word(string)
-  array.string.split(" ")
-  array.each {|string| array << string.reverse}
-  return array.join(" ")
+def reverse_each_word(str)
+  array = str.split(" ")
+  each_array = []
+  collect_array = []
+  array.each {|str| each_array << str.reverse}
+  #return each_array.join(" ")
+  array.collect {|str| collect_array << str.reverse}
+  return collect_array.join(" ")
 end
