39b26bf3d103d5500858686c7d4efe0b98bd9e17
Igor Rud <rud.igor23@gmail.com>
2019-01-29 21:44:33 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+
+def reverse_each_word(sentence1)
+array = sentence1.split(" ")
+returning = []
+array.each do |word|
+returning << word.reverse
+  end
+  returning.join(" ")
+  end
+
+def reverse_each_word(sentence2)
+  array = sentence2.split(" ")
+  returning = []
+  array.collect do |word|
+    returning << word.reverse
+  end
+  returning.join(" ")
+end
