f8ad4dfc5c643403d98871825b2fac6495869390
Daniel Schneider <daschne8@gmail.com>
2019-02-27 00:48:31 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,3 @@
 def reverse_each_word(sentence)
-  array = sentence.split()
-  sentence2 = ""
-  array.collect do |word|
-    sentence2 += word.reverse + " "
-  end
-
-  return sentence2[0..-2]
+  sentence.split.collect { |word| word.reverse}.join(" ")
 end
