54cf51b34c575c829133acb0af7879cae622562b
Michael Chen <michaelchengaming@gmail.com>
2019-03-22 21:37:50 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+=begin
+def reverse_each_word(sentence)
+  new_array = []
+  array = sentence.split
+  array.each do |word|
+    word.reverse!
+    new_array << word
+  end
+  new_array.join(" ")
+end
+=end
+def reverse_each_word(sentence)
+  array = sentence.split
+  array.collect do |word|
+    word.reverse!
+  end
+  array.join(" ")
+end
