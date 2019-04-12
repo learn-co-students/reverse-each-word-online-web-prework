c117de0e3721f7acdd6bbf618c9d826834a9b7cf
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-12 04:37:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+
+def reverse_each_word(word)
+  new_word = word.split(" ")
+  array = []
+  new_word.each do|word|
+  array << word.reverse
+  end
+   array.join(" ")
+end
+
+ def reverse_each_word(word)
+  new_word = word.split(" ")
+  array = []
+  new_word.collect do|word|
+  array << word.reverse
+  end
+   array.join(" ")
+end
