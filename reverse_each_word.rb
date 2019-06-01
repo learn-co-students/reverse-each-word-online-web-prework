8d6b26b3da708134c3c8bd70663407b0f8c9dbe4
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 15:26:20 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 def reverse_each_word(arg)
-  result = []
   my_arr = arg.split
-  my_arr.each do |word|
-    resutl <<< word.reverse
-  end
-  puts result
+  result = my_arr.collect { |n| n.reverse }
+  result.join (" ")
 end
 
-reverse_each_word("Hello there, and how are you?")
+# puts reverse_each_word("Hello there, and how are you?")
