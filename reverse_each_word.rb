773f5f211c0ca1868033fb5fafd1c2b7df6b68b7
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 15:03:19 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(arg)
+  result = []
+  my_arr = arg.split
+  my_arr.each do |word|
+    resutl <<< word.reverse
+  end
+  puts result
+end
+
+reverse_each_word("Hello there, and how are you?")
