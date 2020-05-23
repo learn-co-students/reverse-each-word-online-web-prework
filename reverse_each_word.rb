a310ca3b20ce719ec206aa2e3515b29cf9cbd414
fasnimo <fasnimo@gmail.com>
2020-05-23 16:32:59 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,10 @@
 def reverse_each_word(string)
-  lingo = string.split(" ")
-  new_array = []
- 
-  lingo.collect do |string|
-  new_array << string.reverse
- 
-end 
-new_array.join(" ")
+  phrase = string.split(" ")
+  newArr = []
+  phrase.collect do |element|
+    newArr << element.reverse
+  end 
+  newArr.join(" ")
 end
  
 
